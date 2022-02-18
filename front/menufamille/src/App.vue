6e14d132292012e0263b2226fa02893e13e4df2f
Dorine <dorine.thielemans@hotmail.fr>
2022-02-18 10:02:44 +0100

    setUp base navbar and base app

--- front/menufamille/src/App.vue
+++ front/menufamille/src/App.vue
@@ -1,40 +1,22 @@
 <template>
   <v-app>
-    <v-app-bar
+    <v-navigation-drawer
+      v-model="drawer"
       app
-      color="primary"
-      dark
+      
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-list-item>
+        <router-link to="/">Accueil</router-link>
+      </v-list-item>
+        <v-list-item>
+        <router-link to="/about">Go to About</router-link>
+      </v-list-item>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar app color="#B2DFDB" >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>Menu famille</v-toolbar-title>
     </v-app-bar>
 
     <v-main>      
@@ -47,9 +29,6 @@
 
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
+  data: () => ({ drawer: null }),
 };
 </script>
