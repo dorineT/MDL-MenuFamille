c33f04a318633ab37e6d87e84bda95d6a5748696
David <david.tang@student.unamur.be>
2022-02-22 18:09:36 +0100

    ajout checkboxes periode et personnalisé

--- front/menufamille/src/components/constraintscalendar.vue
+++ front/menufamille/src/components/constraintscalendar.vue
@@ -1,3 +1,39 @@
 <template>
+    <v-container class="checkboxes" fluid>
+     
+    <v-checkbox
+      v-model="checkbox1"
+      :label="`semaine`"
+    ></v-checkbox>
+
+     <v-checkbox
+      v-model="checkbox2"
+      :label="`personalisé`"   
+    ></v-checkbox>
+
     
+  </v-container>
+
+  
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        checkbox1: true,
+        checkbox2: true,
+      }
+    },
+  }
+</script>
+
+<style>
+
+.v-input--selection-controls.v-input {
+    display: inline-block;
+    margin:10px;
+ 
+}
+
+</style>.
---
+++ front/menufamille/src/views/CreationMenu.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <div style="margin: 30px">
+
+        <div class= "créationMenu">
+            <h3> Période :</h3>
+            <constraintscalendar />
+        </div>
+    </div>
+</template>
+
+<script>
+
+import constraintscalendar from '../components/constraintscalendar'
+import Constraintscalendar from '../components/constraintscalendar.vue'
+
+export default {
+    name: 'CreationMenu',
+
+    components: {
+        constraintscalendar,
+    }
+
+        
+    }
+        Constraintscalendar
+
+</script>
