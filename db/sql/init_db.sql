93f74e63eb32e78d6b33214ce67cbd8ba333a2b9
Lesoin <romain.lesoin@student.unamur.be>
2022-02-23 12:44:38 +0100

    ajout des premier trigger vérifier
    suppression en cascade

--- db/sql/init_db.sql
+++ db/sql/init_db.sql
@@ -11,136 +11,136 @@ SET TIMEZONE TO 'UTC+1';
 
 /*** Définition des tables avec leur clé primaire ***/
 CREATE TABLE FAMILLE (
-    id_famille SERIAL NOT NULL ,
-    nom VARCHAR NOT NULL ,
-    code_acces VARCHAR UNIQUE NOT NULL,
-    nb_membres INTEGER NOT NULL,
-    PRIMARY KEY (id_famille)
+                         id_famille SERIAL NOT NULL ,
+                         nom VARCHAR NOT NULL ,
+                         code_acces VARCHAR UNIQUE NOT NULL,
+                         nb_membres INTEGER NOT NULL,
+                         PRIMARY KEY (id_famille)
 );
 
 CREATE TYPE type_menu AS ENUM ('manuel','auto','sugestion');
 CREATE TABLE MENU (
-    id_menu SERIAL NOT NULL ,
-    id_famille INTEGER NOT NULL,
-    periode_debut TIMESTAMP NOT NULL ,
-    periode_fin TIMESTAMP NOT NULL ,
-    plat_identique INTEGER DEFAULT -1,
-    type type_menu,
-    PRIMARY KEY (id_menu)
+                      id_menu SERIAL NOT NULL ,
+                      id_famille INTEGER NOT NULL,
+                      periode_debut TIMESTAMP NOT NULL ,
+                      periode_fin TIMESTAMP NOT NULL ,
+                      plat_identique INTEGER DEFAULT -1,
+                      type type_menu,
+                      PRIMARY KEY (id_menu)
 );
 
 CREATE TABLE MEMBRES (
-    id_membre SERIAL NOT NULL,
-    id_token INTEGER NOT NULL,
-    nom VARCHAR NOT NULL,
-    prenom VARCHAR NOT NULL,
-    email VARCHAR NOT NULL UNIQUE,
-    secret VARCHAR NOT NULL,
-    PRIMARY KEY (id_membre)
+                         id_membre SERIAL NOT NULL,
+                         id_token INTEGER NOT NULL,
+                         nom VARCHAR NOT NULL,
+                         prenom VARCHAR NOT NULL,
+                         email VARCHAR NOT NULL UNIQUE,
+                         secret VARCHAR NOT NULL,
+                         PRIMARY KEY (id_membre)
 );
 
 CREATE TABLE REFRESH_TOKEN (
-    id_refreshToken SERIAL NOT NULL,
-    token VARCHAR NOT NULL,
-    date_expiration TIMESTAMP NOT NULL,
-    date_inscription TIMESTAMP default current_timestamp,
-    PRIMARY KEY (id_refreshToken)
+                               id_refreshToken SERIAL NOT NULL,
+                               token VARCHAR NOT NULL,
+                               date_expiration TIMESTAMP NOT NULL,
+                               date_inscription TIMESTAMP default current_timestamp,
+                               PRIMARY KEY (id_refreshToken)
 );
 
 CREATE TABLE FAMILLE_MEMBRE (
-    id_famille INTEGER NOT NULL ,
-    id_membre INTEGER NOT NULL,
-    PRIMARY KEY (id_famille,id_membre)
+                                id_famille INTEGER NOT NULL ,
+                                id_membre INTEGER NOT NULL,
+                                PRIMARY KEY (id_famille,id_membre)
 );
 
 CREATE TABLE FAVORIS (
-    id_recette INTEGER NOT NULL,
-    id_membre INTEGER NOT NULL,
-    PRIMARY KEY (id_membre,id_recette)
+                         id_recette INTEGER NOT NULL,
+                         id_membre INTEGER NOT NULL,
+                         PRIMARY KEY (id_membre,id_recette)
 );
 
 CREATE TABLE RECETTE (
-    id_recette SERIAL NOT NULL,
-    difficulte INTEGER NOT NULL,
-    calorie INTEGER NOT NULL,
-    temps_cuisson INTEGER NOT NULL,
-    temps_preparation INTEGER NOT NULL,
-    nb_personne INTEGER NOT NULL,
-    nutriscore VARCHAR NOT NULL,
-    preparation TEXT,
-    PRIMARY KEY (id_recette)
+                         id_recette SERIAL NOT NULL,
+                         difficulte INTEGER NOT NULL,
+                         calorie INTEGER NOT NULL,
+                         temps_cuisson INTEGER NOT NULL,
+                         temps_preparation INTEGER NOT NULL,
+                         nb_personne INTEGER NOT NULL,
+                         nutriscore VARCHAR NOT NULL,
+                         preparation TEXT,
+                         PRIMARY KEY (id_recette)
 );
 
 CREATE TABLE RECETTE_TAGS (
-    id_recette INTEGER NOT NULL,
-    id_tag INTEGER NOT NULL,
-    PRIMARY KEY (id_recette,id_tag)
+                              id_recette INTEGER NOT NULL,
+                              id_tag INTEGER NOT NULL,
+                              PRIMARY KEY (id_recette,id_tag)
 );
 
 CREATE TABLE RECETTE_CATEGORIES (
-    id_recette INTEGER NOT NULL,
-    id_categorie INTEGER NOT NULL,
-    PRIMARY KEY (id_recette,id_categorie)
+                                    id_recette INTEGER NOT NULL,
+                                    id_categorie INTEGER NOT NULL,
+                                    PRIMARY KEY (id_recette,id_categorie)
 );
 
 CREATE TYPE periode AS ENUM ('matin','midi','soir');
 CREATE TABLE CATEGORIE (
-    id_categorie SERIAL NOT NULL,
-    periode periode,
-    PRIMARY KEY (id_categorie)
+                           id_categorie SERIAL NOT NULL,
+                           periode periode,
+                           PRIMARY KEY (id_categorie)
 );
 
 CREATE TABLE TAG (
-    id_tag SERIAL NOT NULL,
-    nom VARCHAR UNIQUE NOT NULL ,
-    PRIMARY KEY (id_tag)
+                     id_tag SERIAL NOT NULL,
+                     nom VARCHAR UNIQUE NOT NULL ,
+                     PRIMARY KEY (id_tag)
 );
 
 CREATE TABLE RECETTE_DENREE (
-    id_recette INTEGER NOT NULL,
-    id_denree INTEGER NOT NULL,
-    quantite INTEGER NOT NULL,
-    PRIMARY KEY (id_recette,id_denree)
+                                id_recette INTEGER NOT NULL,
+                                id_denree INTEGER NOT NULL,
+                                quantite INTEGER NOT NULL,
+                                PRIMARY KEY (id_recette,id_denree)
 );
 
 
 CREATE TABLE DENREE (
-    id_denree SERIAL NOT NULL,
-    nom VARCHAR NOT NULL UNIQUE ,
-    nutriscore INTEGER,
-    PRIMARY KEY (id_denree)
+                        id_denree SERIAL NOT NULL,
+                        nom VARCHAR NOT NULL UNIQUE ,
+                        nutriscore INTEGER,
+                        PRIMARY KEY (id_denree)
 );
 
 CREATE TABLE TYPE (
-    id_type SERIAL NOT NULL,
-    nom VARCHAR NOT NULL UNIQUE,
-    PRIMARY KEY (id_type)
+                      id_type SERIAL NOT NULL,
+                      nom VARCHAR NOT NULL UNIQUE,
+                      PRIMARY KEY (id_type)
 );
 
 CREATE TABLE DENREE_TYPE (
-  id_denree INTEGER NOT NULL,
-  id_type INTEGER NOT NULL,
-  PRIMARY KEY (id_denree,id_type)
+                             id_denree INTEGER NOT NULL,
+                             id_type INTEGER NOT NULL,
+                             PRIMARY KEY (id_denree,id_type)
 );
 
 CREATE TABLE CALENDRIER (
-    id_calendrier SERIAL NOT NULL,
-    date TIMESTAMP NOT NULL,
-    nb_personne INTEGER,
-    PRIMARY KEY (id_calendrier)
+                            id_calendrier SERIAL NOT NULL,
+                            date TIMESTAMP NOT NULL,
+                            nb_personne INTEGER,
+                            PRIMARY KEY (id_calendrier)
 );
 
 CREATE TABLE MENU_CALENDRIER (
-    id_menu INTEGER NOT NULL,
-    id_calendrier INTEGER NOT NULL,
-    PRIMARY KEY (id_menu,id_calendrier)
+                                 id_menu INTEGER NOT NULL,
+                                 id_calendrier INTEGER NOT NULL,
+                                 PRIMARY KEY (id_menu,id_calendrier)
 );
 
 CREATE TABLE CALENDRIER_RECETTE (
-    id_calendrier INTEGER NOT NULL,
-    id_recette INTEGER NOT NULL,
-    periode periode,
-    PRIMARY KEY (id_calendrier,id_recette)
+                                    id_calendrier INTEGER NOT NULL,
+                                    id_recette INTEGER NOT NULL,
+                                    periode periode,
+                                    PRIMARY KEY (id_calendrier,id_recette)
 );
 
 /*** Définition des clés étrangères ***/
@@ -186,6 +186,9 @@ INSERT INTO "MenuFamille".tag (id_tag, nom) VALUES (DEFAULT, 'soupe');
 INSERT INTO "MenuFamille".tag (id_tag, nom) VALUES (DEFAULT, 'lunchBox');
 INSERT INTO "MenuFamille".tag (id_tag, nom) VALUES (DEFAULT, 'light');
 INSERT INTO "MenuFamille".tag (id_tag, nom) VALUES (DEFAULT, 'gaterie');
+INSERT INTO "MenuFamille".tag (id_tag, nom) VALUES (DEFAULT, 'vegan');
+INSERT INTO "MenuFamille".tag (id_tag, nom) VALUES (DEFAULT, 'vege');
+
 
 INSERT INTO "MenuFamille".type (id_type, nom) VALUES (DEFAULT, 'fruit');
 INSERT INTO "MenuFamille".type (id_type, nom) VALUES (DEFAULT, 'légume');
@@ -199,3 +202,51 @@ INSERT INTO "MenuFamille".type (id_type, nom) VALUES (DEFAULT, 'sel');
 INSERT INTO "MenuFamille".type (id_type, nom) VALUES (DEFAULT, 'produit sucrée');
 INSERT INTO "MenuFamille".type (id_type, nom) VALUES (DEFAULT, 'matière grasse');
 INSERT INTO "MenuFamille".type (id_type, nom) VALUES (DEFAULT, 'céréale');
+
+
+
+/*** 2.on ne peux plus modifier le menu après validation ou periode debut ***/
+
+
+CREATE OR REPLACE FUNCTION menu_already_valid_FC()
+    RETURNS TRIGGER
+    LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    IF OLD.periode_debut <= NOW() THEN
+        RAISE EXCEPTION 'le menu n est plus modifiable';
+    END IF;
+    RETURN new;
+END;
+$$
+
+
+
+CREATE OR REPLACE FUNCTION menu_calendrier_already_valid_FC()
+    RETURNS TRIGGER
+    LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    IF OLD.menu.periode_debut <= NOW() THEN
+        RAISE EXCEPTION 'vous ne pouvez plus deplacer les jours';
+    END IF;
+    RETURN new;
+END;
+$$
+
+
+
+CREATE TRIGGER menu_already_valid
+    BEFORE UPDATE
+    ON menu
+    FOR EACH ROW
+EXECUTE PROCEDURE menu_already_valid_FC();
+
+
+CREATE TRIGGER menu_calendrier_already_valid
+    BEFORE UPDATE
+    ON menu_calendrier
+    FOR EACH ROW
+EXECUTE PROCEDURE menu_calendrier_already_valid_FC();
