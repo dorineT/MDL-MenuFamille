b91cba217a85af992a3b1068ef362016b5379e44
Aires David <david.airespimentel@gmail.com>
2022-03-08 16:59:54 +0100

    web scraping (colruyt & carrefour)

--- api/menufamille/app/routes/index.js
+++ api/menufamille/app/routes/index.js
@@ -2,6 +2,7 @@
 const type = require('./type.js')
 const user = require('./user.js')
 const famille = require('./famille')
+const product = require('./product');
 const swaggerUi = require('swagger-ui-express'),
 swaggerDocument = require('../../swagger.json');
 module.exports = app => {
@@ -13,5 +14,6 @@ module.exports = app => {
   app.use('/type', type)
   app.use('/user', user)
   app.use('/family', famille)
+  app.use('/product', product)
   // etc..
 }
