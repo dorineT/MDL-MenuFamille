fe41ee1b8db4583d7847e5b3a2c2ad96f14ced4c
Dorine <dorine.thielemans@hotmail.fr>
2022-04-25 19:01:04 +0200

    filtre + fix bug affichage

--- front/menufamille/src/views/Recette.vue
+++ front/menufamille/src/views/Recette.vue
@@ -4,13 +4,67 @@
         <v-container fluid fill-height>
             <v-row justify="center" >
                 <v-col cols="12" sm="12" md="12" lg="10" xl="10">
-                    <v-card >
+                    <v-card class="noPadding">
                         <v-card-title>
-                          Carnet de recettes
+                          <v-row class="justify-end">
+                            <v-col>
+                              Carnet de recettes
+                              
+                            </v-col>
+                            <v-col ><v-btn style="float: right" rounded small class="colorbtnGreen" @click="newRecipe">Nouvelle recette</v-btn></v-col>  
+                          </v-row>
                         </v-card-title>
-                        <v-card-actions  >
-                          <v-btn rounded small class="colorbtnGreen" @click="newRecipe">Nouvelle recette</v-btn>
-                        </v-card-actions>
+                        <v-card-text>                          
+                            <v-row>
+                              <v-col>                             
+                                <v-text-field
+                                  v-model="search"
+                                  append-icon="mdi-magnify"
+                                  label="recherche recette"
+                                  color="orange lighten-2"
+                                  single-line
+                                  hide-details
+                                  @keydown="filter"
+                                  clearable
+                                  @click:clear="() => {search=null; filter()}"
+                                  >
+                                </v-text-field>                        
+                              </v-col>
+                              <v-col>
+                                <v-autocomplete
+                                  v-model="categorieSearch"
+                                  filled                      
+                                  multiple
+                                  rounded
+                                  color="orange lighten-2"
+                                  label="filtre catégorie"
+                                  append-icon="mdi-filter-outline"
+                                  :items="categories"
+                                  @change="filter"
+                                ></v-autocomplete>
+                              </v-col>
+                              <v-col>
+                                <v-autocomplete
+                                  v-model="tagsSearch"
+                                  filled                      
+                                  multiple
+                                  rounded
+                                  color="orange lighten-2"
+                                  label="filtre tags"
+                                  append-icon="mdi-filter-outline"
+                                  :items="tags"
+                                  @change="filter"
+                                ></v-autocomplete>
+                              </v-col>
+                              <v-col>
+                                 <v-checkbox
+                                  v-model="favorisCheckBox"
+                                  label="Favoris"   
+                                  @change="filter"
+                                  ></v-checkbox>
+                              </v-col>
+                            </v-row>                   
+                        </v-card-text>
                     </v-card>
                 </v-col>
 
@@ -50,8 +104,12 @@ import RecetteDAO from '../services/api.recette'
 import RecipeCard from '../components/RecipeCard.vue'
 import DialogNewRecipe from '../components/DialogNewRecipe.vue'
 import FavorisDao from '../services/api.favoris'
+import CategorieDao from '../services/api.categorie'
+import TagDAO from '../services/api.tag'
 let DAORecette = new RecetteDAO()
 let DAOFavoris = new FavorisDao()
+let DAOCategorie = new CategorieDao()
+let DAOTag = new TagDAO()
 
 export default{
     components: {
@@ -59,15 +117,23 @@ export default{
     },
     data(){
         return{
+            recipeAll: [],
             recipe: [],
             favoris: [],
-            showDialogueNewRecipe: false
+            showDialogueNewRecipe: false,
+
+            //filtre
+            search: null,
+            favorisCheckBox: false,
+            tagsSearch: [],
+            categorieSearch: [],
+            tags: [],
+            categories: ['matin', 'midi','soir']
+
         }
     },
-    mounted(){
-        
-      this.fetchRecipe()
-        
+    async mounted(){        
+       this.fetchRecipe()        
     },
     computed: {
         ContainerHeight () {
@@ -86,20 +152,29 @@ export default{
       fetchRecipe(){
         DAORecette.getAll().then(
           (response) => {
-            this.recipe = response.data
-
-            DAOFavoris.getAll().then(
+            var recettesRes = response.data            
+             DAOFavoris.getAll().then(
               (response)=>{
-                this.favoris = response.data                             
-                this.recipe.forEach(element => {
-                  let isIn = this.favoris.some(id => id.id_recette === element.id_recette)
+                this.favoris = response.data 
+                
+                for(let i =0; i< recettesRes.length ; i++){
+                  let isIn = this.favoris.some(id => id.id_recette === recettesRes[i].id_recette)
                   if(isIn){
-                    element.isFavoris = 1
+                    recettesRes[i].isFavoris = 1
                   }else{
-                    element.isFavoris = 0
+                    recettesRes[i].isFavoris = 0
                   }
-                   
-                });
+                   //get tags
+
+                      recettesRes[i].tags.forEach( tag => {
+                      if(!this.tags.includes(tag.nom)){
+                        this.tags.push(tag.nom)
+                      }
+                    })
+                  this.recipe = recettesRes
+                  this.recipeAll = recettesRes
+                }
+            
               }
             )
           }
@@ -119,6 +194,25 @@ export default{
       updateList(idRecette, favoris){
         let recette = this.recipe.find( el => el.id_recette === idRecette)
         recette.isFavoris = favoris
+      },
+
+      //filter
+      filter(){         
+          this.recipe = this.recipeAll.filter((elem) => (!this.favorisCheckBox) || elem.isFavoris === 1 )
+          .filter((elem)=> (! (this.search !== null)) || elem.nom.toLowerCase().match(this.search.toLowerCase()))
+          .filter((elem) => (! (this.categorieSearch.length > 0) || this.filterCategories(elem)))  
+          .filter((elem) => (! (this.tagsSearch.length > 0) || this.filterTags(elem)))         
+
+      },
+      filterCategories(elem){  
+        let catElem = []
+        elem.categories.forEach(el => {catElem.push(el.periode)})
+        return this.categorieSearch.every(el => catElem.includes(el))
+      },
+      filterTags(elem){  
+        let tagElem = []
+        elem.tags.forEach(el => {tagElem.push(el.nom)})
+        return this.tagsSearch.every(el => tagElem.includes(el))
       }
     },
 }
